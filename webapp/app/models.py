aa5363bd6c63143f230ee8e169a7338760277c6c
Jonah Lloyd <sc16jl@leeds.ac.uk>
2018-02-07 14:19:57 +0000

    database uml diagram and inital start to database code

---
+++ webapp/app/models.py
@@ -0,0 +1,24 @@
+class UserInfo(db.Model):
+
+  __tablename__ = 'userinfo'
+
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(100), unique=True)
+  password = db.Column(db.String(100))
+  forename = db.Column(db.String(100))
+  surname = db.Column(db.String(100))
+  card_number = db.Column(db.Integer(16))
+  cvc = db.Column(db.Integer(3))
+  expiry_date = db.Column(db.Integer(4))
+
+  def __init__(self,email,password,forename,surname,card_number,cvc,expiry_date):
+      self.email = email
+      self.password = password
+      self.forename = forename
+      self.surname = surname
+      self.card_number = card_number
+      self.cvc = cvc
+      self.expiry_date = expiry_date
+
+  def __repr__(self):
+      return '<User %r>' % (self.email)
