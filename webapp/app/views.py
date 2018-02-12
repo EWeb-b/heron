00704065904854d4948ab675808e068daf17fc88
Graeme Cross <sc15gpc@leeds.ac.uk>
2018-02-12 16:28:15 +0000

    Added doctrings to the api methods

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -147,19 +147,20 @@ def change_password():
             return redirect('/change_password')
 
 
-@app.route('/api/hello', methods=['GET'])
-def apiHelloWorld():
-    return jsonify({"Hello": "World"})
-
-
 @app.route('/api/movies', methods=['GET'])
 def apiGetMovies():
+    """Returns all films in then database in JSON format
+
+    Returns: A JSON object containing details of all films in the database
+    """
     movies = FilmDetails.query.all()
     return jsonify(FilmDetails.serializeList(movies))
 
 
 @app.route('/api/movies/add', methods=['POST'])
 def apiNewMovie():
+    """Adds a new film to the database via a POST request containing JSON data
+    for the new movie"""
     if not request.json or 'film_name' not in request.json:
         abort(404)
     movie = FilmDetails(**request.json)
