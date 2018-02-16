5704c453a7ef47950ff3ace1c8cb303794dfd564
Graeme Cross <sc15gpc@leeds.ac.uk>
2018-02-16 09:31:33 +0000

    Updated the app to display the blub for the selected movie

--- managerApp/ManagerApp.py
+++ managerApp/ManagerApp.py
@@ -1,9 +1,8 @@
 import sys
 import requests
 from PyQt5.QtWidgets import (
-    QWidget, QPushButton, QHBoxLayout, QComboBox,
+    QWidget, QPushButton, QHBoxLayout, QComboBox, QLabel,
     QVBoxLayout, QApplication, QMainWindow)
-from PyQt5.QtCore import pyqtSlot
 
 
 server = 'http://localhost:5000'
@@ -15,29 +14,44 @@ class ManagerApp(QMainWindow):
         super(ManagerApp, self).__init__()
         self.setUpUi()
 
-    @pyqtSlot()
     def downloadMovies(self):
-        self.movieData = requests.get(server + '/api/movies').json()
+        self.movieCombo.clear()
+        self.movieData = requests.get(server + '/api/movies').json()['films']
         self.movieCombo.addItems(
-            [movie['film_name'] for movie in self.movieData['films']])
+            [movie['film_name'] for movie in self.movieData])
+        self.movieLbl.setText(self.movieData[0]['blurb'])
+
+    def movieComboUpdate(self, str):
+        data = next(
+            (movie for movie in self.movieData
+                if movie['film_name'] == str), None)
+        self.movieLbl.setText(data['blurb'])
 
     def setUpUi(self):
+        # Layouts
         mainLayout = QVBoxLayout()
         upperBox = QHBoxLayout()
         midBox = QHBoxLayout()
         lowBox = QHBoxLayout()
-
-        self.movieCombo = QComboBox()
-
-        updatebtn = QPushButton("Update")
-        updatebtn.clicked.connect(self.downloadMovies)
-        upperBox.addWidget(self.movieCombo)
-        upperBox.addWidget(updatebtn)
-
         mainLayout.addLayout(upperBox)
         mainLayout.addLayout(midBox)
         mainLayout.addLayout(lowBox)
 
+        # Combobox for the movie names
+        self.movieCombo = QComboBox()
+        self.movieCombo.activated[str].connect(self.movieComboUpdate)
+
+        # updateBtn downloads data from the server
+        updateBtn = QPushButton("Update")
+        updateBtn.clicked.connect(self.downloadMovies)
+        upperBox.addWidget(self.movieCombo)
+        upperBox.addWidget(updateBtn)
+
+        # Lable to display the blurb info
+        self.movieLbl = QLabel()
+        midBox.addWidget(self.movieLbl)
+
+        # QMainWindow needs a widget to add a layout to
         widget = QWidget()
         self.setCentralWidget(widget)
         widget.setLayout(mainLayout)
@@ -45,6 +59,7 @@ class ManagerApp(QMainWindow):
         self.setGeometry(300, 300, 500, 300)
         self.setWindowTitle('Cinema Management Tool')
         self.show()
+
 
 app = QApplication(sys.argv)
 GUI = ManagerApp()
