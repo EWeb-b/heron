e07b218213c37aee91e21ce9ad105edb51a1c970
Graeme Cross <sc15gpc@leeds.ac.uk>
2018-02-26 15:02:27 +0000

    changed config to objact based to allow multiple configs using inheritance

--- webapp/config.py
+++ webapp/config.py
@@ -1,10 +1,11 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+class Config(object):
+    WTF_CSRF_ENABLED = True
+    SECRET_KEY = 'Graeme-smells-of-cheese'
+    basedir = os.path.abspath(os.path.dirname(__file__))
 
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'Graeme-smells-of-cheese'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
