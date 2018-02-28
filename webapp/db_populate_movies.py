d8aebe8c07fe6f9c606ef6fdd1f5529c4accdf67
Jack Boreham <sc16jeb@leeds.ac.uk>
2018-02-28 17:29:20 +0000

    bringing other files uptodate to continue test development

--- webapp/db_populate_movies.py
+++ webapp/db_populate_movies.py
@@ -5,27 +5,30 @@ import json
 
 sampleData = [
     {
-      "filmCertificate": 12,
-      "filmBlurb": "Home alone, but on mars.",
-      "filmDirector": "Ridley Scott",
-      "filmName": "The Martian",
-      "filmActor": "Matt Damon"
+        << << << < 0cda86d41cca84e4915db9f9e885404d674d4dac
+        "filmCertificate": 12,
+        "filmBlurb": "Home alone, but on mars.",
+        "filmDirector": "Ridley Scott",
+        "filmName": "The Martian",
+        "filmActor": "Matt Damon"
     },
     {
-      "filmCertificate": 1,
-      "filmBlurb": "Professor has unusually productive sabatical.",
-      "filmDirector": "Steven Spielberg",
-      "filmName": "Raiders of the Lost Ark",
-      "filmActor": "Harrison Ford"
+        "filmCertificate": 1,
+        "filmBlurb": "Professor has unusually productive sabatical.",
+        "filmDirector": "Steven Spielberg",
+        "filmName": "Raiders of the Lost Ark",
+        "filmActor": "Harrison Ford"
     },
     {
-      "filmCertificate": 15,
-      "filmBlurb": "Professor finds a shiny head.",
-      "filmDirector": "Steven Spielberg",
-      "filmName": "Indiana Jones and the Kingdom of the Crystal Skull",
-      "filmActor": "Harrison Ford"
+        "filmCertificate": 15,
+        "filmBlurb": "Professor finds a shiny head.",
+        "filmDirector": "Steven Spielberg",
+        "filmName": "Indiana Jones and the Kingdom of the Crystal Skull",
+        "filmActor": "Harrison Ford"
     }
-    ]
+]
+== == == =
+>>>>>> > bringing other files uptodate to continue test development
 
 for movie in sampleData:
     newMovie = FilmDetails(**movie)
