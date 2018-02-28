0630174b6b89cd466731fb289920239edd75ed06
Jack Boreham <sc16jeb@leeds.ac.uk>
2018-02-28 18:14:34 +0000

    Added test config, Testing login response

--- webapp/logintest.py
+++ webapp/logintest.py
@@ -3,29 +3,31 @@ import unittest
 from flask import Flask, request
 from flask_testing import TestCase
 from flask_login import LoginManager, current_user, AnonymousUserMixin
-from app.models import User
+from app.models import Account
 
 
 class Anonymous(AnonymousUserMixin):
+
     def __init__(self):
-        self.username = 'Steve'
+        self.email = 'jack@yahooo.com'
+
+# Set up test user
 
 
 class BaseTestCase(TestCase):
 
-
     def create_app(self):
-       app=Flask(_name_)
-       app.config.from_object('config.TestConfig')
-       db.init_app(app)
-       login_manager = LoginManager()
-       login_manager.init_app(app)
-       login_manager.anonymous_user = Anonymous
-       return app
+        app = Flask(_name_)
+        app.config.from_object('config.TestConfig')
+        db.init_app(app)
+        login_manager = LoginManager()
+        login_manager.init_app(app)
+        login_manager.anonymous_user = Anonymous
+        return app
 
     def setUp(self):
         db.create_all()
-        db.session.add(User(username='Steve', password='password'))
+        db.session.add(Account(email='jack@yahooo.com', password='password'))
         db.session.commit()
 
     def tearDown(self):
@@ -33,41 +35,35 @@ class BaseTestCase(TestCase):
         db.drop_all()
 
 
-
-
 class FlaskTestCase(BaseTestCase):
 
-    #ensure flask set up correctly
+    # ensure flask set up correctly
     def test_index(self):
         tester = app.test_client(self)
-        response = tester.get('/login', content_type='html/text')
+        response = tester.get('/home', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
-
-    #ensure login page loads
+    # ensure login page loads
     def test_login_page_loads(self):
         tester = app.test_client(self)
         response = tester.get('/login', content_type='html/text')
         self.assertTrue(b'Log into Your Account' in response.data)
 
-
-    #ensure login works with correct account
+    # ensure login works with correct account
     def test_working_login(self):
         tester = app.test_client(self)
-        response = tester.post('/login', data=dict(username='Steve', password='password'), follow_redirects=True)
-        self.assertTrue(b'Account' in response.data)
+        response = tester.post(
+            '/login', data=dict(email='jack@yahooo.com', password='password'), follow_redirects=True)
+        self.assertTrue(b'' in response.data)
 
-    #ensure user can register
+    # ensure user can register
     def test_registration(self):
         with self.client:
             tester = app.test_client(self)
             response = tester.post('create_account/', data=dict(
-                username='Steve', password='flask'), follow_redirects=True)
-            self.assertTrue(current_user.username == 'Steve')
+                email='jack@yahooo.com', password='password'), follow_redirects=True)
+            self.assertTrue(current_user.email == 'jack@yahooo.com')
 
 
-
-
-
-if _name__=='__main_':
+if __name__ == '__main_':
     unittest.main()
