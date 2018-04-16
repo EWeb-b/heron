a1402210fc33f52ec4aed203c66f50418f35a3a2
James Dean <ll14j3d@leeds.ac.uk>
2018-04-16 16:56:29 +0000

    edited to work with 5 ticket types rather than 2

--- task4/weeklyBreakDown.py
+++ task4/weeklyBreakDown.py
@@ -6,14 +6,17 @@ from collections import namedtuple
 def weeklyBAR(date):
     n_groups = 3
 
-    means_men = [20, 35, 30]
+    ADULT = [20, 35, 30]
 
 
-    means_women = [25, 32, 34]
+    STUDENT = [25, 32, 34]
 
 
-    means_cat = [40, 25, 32]
+    CHILD = [40, 25, 32]
 
+    OAP = [10, 15, 17]
+
+    VIP = [5, 2, 10]
 
     fig, ax = plt.subplots()
 
@@ -23,25 +26,33 @@ def weeklyBAR(date):
     opacity = 0.4
     error_config = {'ecolor': '0.3'}
 
-    rects1 = ax.bar(index, means_men, bar_width,
+    rects1 = ax.bar(index, ADULT, bar_width,
                      color='b',
                     error_kw=error_config,
                     label='Adult')
 
-    rects2 = ax.bar(index + bar_width, means_women, bar_width,
+    rects2 = ax.bar(index + bar_width, STUDENT, bar_width,
                      color='r',
                     error_kw=error_config,
                     label='Student')
 
-    rects3 = ax.bar(index + 2*bar_width, means_cat, bar_width,
+    rects3 = ax.bar(index + 2*bar_width, CHILD, bar_width,
                      color='g',
                     error_kw=error_config,
                     label='Child')
+    rects4 = ax.bar(index + 3*bar_width, OAP, bar_width,
+                     color='y',
+                    error_kw=error_config,
+                    label='OAP')
+    rects5 = ax.bar(index + 4*bar_width, VIP, bar_width,
+                     color='maroon',
+                    error_kw=error_config,
+                    label='VIP')
 
     ax.set_xlabel('Movies')
     ax.set_ylabel('Ticket sales')
     ax.set_title('Daily breakdown for: '+date)
-    ax.set_xticks(index + bar_width)
+    ax.set_xticks(index + 2*bar_width)
     ax.set_xticklabels(['Black Panther', 'The Sound of Water', 'The Greatest Showman'])
     ax.legend()
 
