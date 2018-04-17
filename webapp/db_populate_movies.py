fdd6a2354046498e7b27ef169edda594b2e8514e
Jack Boreham <sc16jeb@leeds.ac.uk>
2018-04-17 21:42:40 +0100

    sample ticket data added for manager app testing

--- webapp/db_populate_movies.py
+++ webapp/db_populate_movies.py
@@ -3,38 +3,155 @@ from app.models import FilmDetails
 from app import app, db, models
 import json
 
-sampleData = [
+filmData = [
     {
-        "filmCertificate": 12,
+        "id": 1,
+        "filmCertificateId": 12,
         "filmBlurb": "Entering dreams.",
         "filmDirector": "Christopher Nolan",
         "filmName": "Inception",
         "filmActor": "Leonardo DiCaprio"
     },
     {
-        "filmCertificate": 12,
+        "id": 2,
+        "filmCertificateId": 12,
         "filmBlurb": "Home alone, but on mars.",
         "filmDirector": "Ridley Scott",
         "filmName": "The Martian",
         "filmActor": "Matt Damon"
     },
     {
-        "filmCertificate": 12,
+        "id": 3,
+        "filmCertificateId": 12,
         "filmBlurb": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival",
         "filmDirector": "Christopher Nolan",
         "filmName": "Interstellar",
         "filmActor": "Matthew McConaughey"
     },
     {
-        "filmCertificate": 18,
-        "filmBlurb": "Post apocolyptic madness",
-        "filmDirector": "George Miller",
-        "filmName": "Mad Max: Fury Road",
-        "filmActor": "Tom Hardy"
+        "id": 4,
+        "filmCertificateId": 12,
+        "filmBlurb": "Peruvain Bear up to no good",
+        "filmDirector": "Paul King",
+        "filmName": "Paddington 2",
+        "filmActor": "Ben Wishaw"
+    },
+    {
+        "id": 5,
+        "filmCertificateId": 15,
+        "filmBlurb": "Fishman seduces silent woman",
+        "filmDirector": "Guillermo del Toro",
+        "filmName": "The Shape of Water",
+        "filmActor": "Sally Hawkins"
+    },
+    {
+        "id": 6,
+        "filmCertificateId": 12,
+        "filmBlurb": "",
+        "filmDirector": "Ryan Coogler",
+        "filmName": "Black Panther",
+        "filmActor": "Chadwick Boseman"
+    },
+    {
+        "id": 7,
+        "filmCertificateId": 12,
+        "filmBlurb": "Hugh Jackman's career takes a suspect turn",
+        "filmDirector": "Michael Gracey",
+        "filmName": "The Greatest  Showman",
+        "filmActor": "Hugh Jackman"
+    },
+    {
+        "id": 8,
+        "filmCertificateId": 12,
+        "filmBlurb": "Horrible remake",
+        "filmDirector": "Jake Kasden",
+        "filmName": "Jumanji: Welcome to the jungle",
+        "filmActor": "Dwayne Johnson"
+    },
+    {
+        "id": 9,
+        "filmCertificateId": 12,
+        "filmBlurb": "Undead reconcilliation",
+        "filmDirector": "Lee Unkrich",
+        "filmName": "CoCo",
+        "filmActor": "Anthony Gonzalez"
     }
 ]
 
-for movie in sampleData:
+for movie in filmData:
     newMovie = FilmDetails(**movie)
     db.session.add(newMovie)
+    db.session.commit()
+
+ticketData = [
+
+    {
+        "id": 1,
+        "ownerProfileId": 1,
+        "ticketTypeId": 1,
+        "ticketScreeningId": 1,
+        "ticketDateBought": 2018 - 14 - 01 15: 01: 10,
+
+    },
+    {
+        "id": 2,
+        "ownerProfileId": 1,
+        "ticketTypeId": 1,
+        "ticketScreeningId": 1,
+        "ticketDateBought": 2018 - 14 - 01 15: 01: 10,
+
+    },
+    {
+        "id": 3,
+        "ownerProfileId": 1,
+        "ticketTypeId": 1,
+        "ticketScreeningId": 1,
+        "ticketDateBought": 2018 - 14 - 01 15: 01: 10,
+
+    },
+    {
+        "id": 4,
+        "ownerProfileId": 2,
+        "ticketTypeId": 2,
+        "ticketScreeningId": 1,
+        "ticketDateBought": 2018 - 14 - 01 16: 01: 10,
+
+    },
+    {
+        "id": 5,
+        "ownerProfileId": 2,
+        "ticketTypeId": 2,
+        "ticketScreeningId": 1,
+        "ticketDateBought": 2018 - 14 - 01 16: 01: 10,
+
+    },
+    {
+        "id": 6,
+        "ownerProfileId": 3,
+        "ticketTypeId": 1,
+        "ticketScreeningId": 1,
+        "ticketDateBought": 2018 - 14 - 01 17: 01: 10,
+
+    },
+    {
+        "id": 7,
+        "ownerProfileId": 3,
+        "ticketTypeId": 1,
+        "ticketScreeningId": 1,
+        "ticketDateBought": 2018 - 14 - 01 17: 01: 10,
+
+    },
+    {
+        "id": 8,
+        "ownerProfileId": 3,
+        "ticketTypeId": 1,
+        "ticketScreeningId": 1,
+        "ticketDateBought": 2018 - 14 - 01 17: 01: 10,
+
+    }
+]
+
+for aTicket in ticketData:
+    newTicket = Ticket(**aTicket)
+    db.session.add(newTicket)
     db.session.commit()
