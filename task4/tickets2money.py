a0a09bf893bee3bd880f215167305f9a1f24ab64
James Dean <ll14j3d@leeds.ac.uk>
2018-04-25 20:54:54 +0000

    Replace tickets2money.py

--- task4/tickets2money.py
+++ task4/tickets2money.py
@@ -1,12 +1,24 @@
-class tickets2money():
-    # this class is used to convert the number of particular tickets sold, into revenue to be used for tables and plotting
-    def adult(num):
-        return num*5.0
-    def student(num):
-        return num*4.0
-    def child(num):
-        return num*4.0
-    def OAP(num):
-        return num*4.0
-    def VIP(num):
-        return num*6.0
+def adult(num):
+    return num*5.0
+def student(num):
+    return num*4.0
+def child(num):
+    return num*4.0
+def OAP(num):
+    return num*4.0
+def VIP(num):
+    return num*6.0
+
+def Total(tickets_sold):
+    # takes list of length 5. Each index corresponds to the type of ticket, the int inside is quantity
+    # index0: adult
+    # index1: student
+    # index2: child
+    # index3: OAP
+    # index4: VIP
+    #total = adult(tickets_sold[0]) + student(tickets_sold[1]) + child(tickets_sold[2]) + OAP(tickets_sold[3]) + VIP(tickets_sold[4])
+    total = OAP(tickets_sold[0]) + adult(tickets_sold[1]) + student(tickets_sold[2]) + child(tickets_sold[3]) + VIP(tickets_sold[4])
+    return total
+
+
+#print(Total([1,1,1,1,1]))
